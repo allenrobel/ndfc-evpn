09eeb62e35c6af2970fe7967c2a6386cb5796bd4
Allen Robel <arobel@cisco.com>
2023-06-26 13:30:09 -1000

    Edits for easier maintainability

--- ContainerLab/host-config.sh
+++ ContainerLab/host-config.sh
@@ -1,25 +1,68 @@
-#!/bin/sh
-sudo docker exec -d clab-nxos-host_1411 ip link set eth1 up
-sudo docker exec -d clab-nxos-host_1411 ip link add link eth1 name Vlan2300 type vlan id 2300
-sudo docker exec -d clab-nxos-host_1411 ip addr add 192.168.10.201/24 brd 192.168.10.255 dev Vlan2300
-sudo docker exec -d clab-nxos-host_1411 ip link set dev Vlan2300 up
-sudo docker exec -d clab-nxos-host_1411 ip route add 192.168.11.0/24 via 192.168.10.1
+#!/usr/bin/bash
+# Configure host container interfaces and routes.
+# Host containers are each connected to n9000v leafs, via the leaf's
+# Ethernet1/64 interface.  Ethernet1/64 is configured as a trunk with
+# allowed-vlan 2300 (VRF Clients) or 2301 (VRF Servers).  This script
+# configures the host container eth1 interface with a dot1q configuration
+# to match the respective leaf interface config.
 
-sudo docker exec -d clab-nxos-host_1412 ip link set eth1 up
-sudo docker exec -d clab-nxos-host_1412 ip link add link eth1 name Vlan2300 type vlan id 2300
-sudo docker exec -d clab-nxos-host_1412 ip addr add 192.168.10.202/24 brd 192.168.10.255 dev Vlan2300
-sudo docker exec -d clab-nxos-host_1412 ip link set dev Vlan2300 up
-sudo docker exec -d clab-nxos-host_1412 ip route add 192.168.11.0/24 via 192.168.10.1
+VLAN_CLIENTS=2300
+VLAN_CLIENTS_GATEWAY="192.168.10.1"
+VLAN_CLIENTS_ROUTE="192.168.10.0/24"
+VLAN_CLIENTS_BROADCAST="192.168.10.255"
 
-sudo docker exec -d clab-nxos-host_2411 ip link set eth1 up
-sudo docker exec -d clab-nxos-host_2411 ip link add link eth1 name Vlan2301 type vlan id 2301
-sudo docker exec -d clab-nxos-host_2411 ip addr add 192.168.11.203/24 brd 192.168.11.255 dev Vlan2301
-sudo docker exec -d clab-nxos-host_2411 ip link set dev Vlan2301 up
-sudo docker exec -d clab-nxos-host_2411 ip route add 192.168.10.0/24 via 192.168.11.1
+VLAN_SERVERS=2301
+VLAN_SERVERS_GATEWAY="192.168.11.1"
+VLAN_SERVERS_ROUTE="192.168.11.0/24"
+VLAN_SERVERS_BROADCAST="192.168.11.255"
 
-sudo docker exec -d clab-nxos-host_2412 ip link set eth1 up
-sudo docker exec -d clab-nxos-host_2412 ip link add link eth1 name Vlan2301 type vlan id 2301
-sudo docker exec -d clab-nxos-host_2412 ip addr add 192.168.11.204/24 brd 192.168.11.255 dev Vlan2301
-sudo docker exec -d clab-nxos-host_2412 ip link set dev Vlan2301 up
-sudo docker exec -d clab-nxos-host_2412 ip route add 192.168.10.0/24 via 192.168.11.1
+VLAN=${VLAN_CLIENTS}
+ROUTE=${VLAN_SERVERS_ROUTE}
+GATEWAY=${VLAN_CLIENTS_GATEWAY}
+BROADCAST=${VLAN_CLIENTS_BROADCAST}
 
+HOST=clab-nxos-host_1411
+IP="192.168.10.201/24"
+echo $HOST Vlan${VLAN} configuration START
+sudo docker exec -d $HOST ip link set eth1 up
+sudo docker exec -d $HOST ip link add link eth1 name Vlan${VLAN} type vlan id ${VLAN}
+sudo docker exec -d $HOST ip addr add ${IP} brd ${BROADCAST} dev Vlan${VLAN}
+sudo docker exec -d $HOST ip link set dev Vlan${VLAN} up
+sudo docker exec -d $HOST ip route add ${ROUTE} via ${GATEWAY}
+echo $HOST Vlan${VLAN} configuration DONE
+
+
+HOST=clab-nxos-host_1412
+IP="192.168.10.202/24"
+echo $HOST Vlan${VLAN} configuration START
+sudo docker exec -d $HOST ip link set eth1 up
+sudo docker exec -d $HOST ip link add link eth1 name Vlan${VLAN} type vlan id ${VLAN}
+sudo docker exec -d $HOST ip addr add ${IP} brd ${BROADCAST} dev Vlan${VLAN}
+sudo docker exec -d $HOST ip link set dev Vlan${VLAN} up
+sudo docker exec -d $HOST ip route add ${ROUTE} via ${GATEWAY}
+echo $HOST Vlan${VLAN} configuration DONE
+
+VLAN=${VLAN_SERVERS}
+ROUTE=${VLAN_CLIENTS_ROUTE}
+GATEWAY=${VLAN_SERVERS_GATEWAY}
+BROADCAST=${VLAN_SERVERS_BROADCAST}
+
+HOST=clab-nxos-host_2411
+IP="192.168.11.203/24"
+echo $HOST Vlan${VLAN} configuration START
+sudo docker exec -d $HOST ip link set eth1 up
+sudo docker exec -d $HOST ip link add link eth1 name Vlan${VLAN} type vlan id ${VLAN}
+sudo docker exec -d $HOST ip addr add ${IP} brd ${BROADCAST} dev Vlan${VLAN}
+sudo docker exec -d $HOST ip link set dev Vlan${VLAN} up
+sudo docker exec -d $HOST ip route add ${ROUTE} via ${GATEWAY}
+echo $HOST Vlan${VLAN} configuration DONE
+
+HOST=clab-nxos-host_2412
+IP="192.168.11.204/24"
+echo $HOST Vlan${VLAN} configuration START
+sudo docker exec -d $HOST ip link set eth1 up
+sudo docker exec -d $HOST ip link add link eth1 name Vlan${VLAN} type vlan id ${VLAN}
+sudo docker exec -d $HOST ip addr add ${IP} brd ${BROADCAST} dev Vlan${VLAN}
+sudo docker exec -d $HOST ip link set dev Vlan${VLAN} up
+sudo docker exec -d $HOST ip route add ${ROUTE} via ${GATEWAY}
+echo $HOST Vlan${VLAN} configuration DONE
